7d204f09548e4f33ff102a81bafd7153c6fbb7b1
Paweł Taborowski <peantab@gmail.com>
2019-04-13 21:43:18 +0200

    Initial logger project and runtime

---
+++ logger/src/pl/edu/agh/iisg/ewolucja/logger/Logger.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.iisg.ewolucja.logger;
+
+public class Logger {
+	Logger(){}
+	
+	void startWork() {}
+	
+	void endWork() {}
+	
+	void log(String json) {}
+	
+	void logStructured(int a, int b, int c) {}
+}
