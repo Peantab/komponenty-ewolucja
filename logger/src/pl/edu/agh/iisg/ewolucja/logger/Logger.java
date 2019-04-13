a06e12a1e6523fd40841d8a6cabab91137dcd726
Paweł Taborowski <peantab@gmail.com>
2019-04-13 22:33:03 +0200

    Fixed mock, added entries for .gitignore

--- logger/src/pl/edu/agh/iisg/ewolucja/logger/Logger.java
+++ logger/src/pl/edu/agh/iisg/ewolucja/logger/Logger.java
@@ -1,13 +1,23 @@
 package pl.edu.agh.iisg.ewolucja.logger;
 
 public class Logger {
-	Logger(){}
+	public Logger(){
+		System.out.println("Started mock logger.");
+	}
 	
-	void startWork() {}
-	
-	void endWork() {}
-	
-	void log(String json) {}
-	
-	void logStructured(int a, int b, int c) {}
+	public void startWork() {
+		System.out.println("Mock logger started work.");
+	}
+
+	public void endWork() {
+		System.out.println("Mock logger finished work.");
+	}
+
+	public void log(String json) {
+		System.out.println("Mock-logged: " + json);
+	}
+
+	public void logStructured(int a, int b, int c) {
+		System.out.println("Mock-logged: " + a + b + c);
+	}
 }
