27ee160476ded83137eb041e041bf911e3486d9e
imthumbelina <aga.zmc@gmail.com>
2019-04-14 14:25:00 +0200

    Add package

---
+++ logReader/logReaderLib/src/main/java/pl/edu/agh/iisg/ewolucja/logreader/LogReader.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.iisg.ewolucja.logreader;
+
+import java.time.Instant;
+import java.util.List;
+
+public class LogReader {
+    public List<String> getLogs(int experimentId){
+        return null;
+    }
+    public List<String> getLastLogs(int count){
+        return null;
+    }
+    public List<String> getLogs(Instant timeFrom, Instant timeTo){
+        return null;
+    }
+    public List<String> getLogs(String moduleName){
+        return null;
+    }
+}
