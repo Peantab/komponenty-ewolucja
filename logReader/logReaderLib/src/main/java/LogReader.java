71dd6a437e68f44a8fa1cc5d1939aefead7ed944
imthumbelina <aga.zmc@gmail.com>
2019-04-14 13:53:11 +0200

    Add logReaderLib directory

---
+++ logReader/logReaderLib/src/main/java/LogReader.java
@@ -0,0 +1,17 @@
+import java.time.Instant;
+import java.util.List;
+
+public class LogReader {
+    List<String> getLogs(int experimentId){
+        return null;
+    }
+    List<String> getLastLogs(int count){
+        return null;
+    }
+    List<String> getLogs(Instant timeFrom, Instant timeTo){
+        return null;
+    }
+    List<String> getLogs(String moduleName){
+        return null;
+    }
+}
