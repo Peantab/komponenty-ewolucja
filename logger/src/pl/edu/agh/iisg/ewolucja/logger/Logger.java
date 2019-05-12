ff255f96100a7e42e8946ef92faabc8c17391c7a
Paweł Taborowski <peantab@gmail.com>
2019-05-12 19:35:55 +0200

    Logger further development
    * updated POM file to contain Apache Felix's Bundle Plugin
    * updated POM file to contain dependency to ElasticSearch
    * implemented constructor and log function for Logger.java
    * updated .gitignore to simplify storage of ElasticSearch

--- logger/src/pl/edu/agh/iisg/ewolucja/logger/Logger.java
+++ logger/src/pl/edu/agh/iisg/ewolucja/logger/Logger.java
@@ -1,7 +1,19 @@
 package pl.edu.agh.iisg.ewolucja.logger;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
 public class Logger {
+	private int idCounter = 0;
+	private final RestHighLevelClient client;
 	public Logger(){
+		client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
 		System.out.println("Started mock logger.");
 	}
 	
@@ -13,8 +25,11 @@ public class Logger {
 		System.out.println("Mock logger finished work.");
 	}
 
-	public void log(String json) {
-		System.out.println("Mock-logged: " + json);
+	public void log(String json) throws IOException {
+		IndexRequest request = new IndexRequest("temporaryIndexName");
+		request.id(String.valueOf(++idCounter));
+		request.source(json, XContentType.JSON);
+		client.index(request, RequestOptions.DEFAULT);
 	}
 
 	public void logStructured(int a, int b, int c) {
