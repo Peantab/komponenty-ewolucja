13056e24f000eca92bdaa028c17cce6f7cb7a5f1
MarekGn <37642342+MarekGn@users.noreply.github.com>
2019-06-02 00:44:37 +0200

    Add configuration and population initiation

--- main/src/pl/edu/agh/iisg/ewolucja/main/Main.java
+++ main/src/pl/edu/agh/iisg/ewolucja/main/Main.java
@@ -1,11 +1,22 @@
 package pl.edu.agh.iisg.ewolucja.main;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
 
 	private JSONArray jsonArray;
 	private String problemType;
 	private int iterations;
 	private Operator operator;
+	private Configuration configuration;
+	private Population population;
+	private int genotypeSize;
 
 	public Main(JSONArray jsonArray) {
 		this.jsonArray = jsonArray;
@@ -13,10 +24,56 @@ public class Main {
 			JSONObject obj = jsonArray.getJSONObject(i);
 			if(obj.getString("name").equals("GLOBALINFO")){
 				this.problemType = obj.getString("problemType");
-				this.iterations = obj.getInt("iterations")
+				this.iterations = obj.getInt("iterations");
+			} else if(obj.getString("name").equals("CONFIGURATION")){
+				this.genotypeSize = obj.getInt("genotype.size");
+				configInit(obj);
+			} else if(obj.getString("name").equals("POPULATION")){
+				populationInit(obj);
 			} else {
-				step(problemType, iterations, obj)
+				step(problemType, iterations, obj);
 			}
+		}
+	}
+
+	private void configInit(JSONObject obj){
+		Map<String,Object> configMap = new HashMap<String, Object>();
+
+		configMap.put("individuals.selected", obj.getInt("individuals.selected"));
+		configMap.put("problem.crossoverProbability", obj.getDouble("problem.crossoverProbability"));
+		configMap.put("population.size", obj.getInt("population.size"));
+		configMap.put("genotype.size", obj.getInt("genotype.size"));
+		configMap.put("problem.mutationProbability", obj.getDouble("problem.mutationProbability"));
+
+		this.configuration = new Configuration(configMap);
+	}
+
+	private void populationInit(JSONObject obj){
+		if (this.problemType.equals("BINARY")){
+			// Creating ArrayList of Genes
+			ArrayList<Gene> genes = new ArrayList<>();
+			JSONArray jgenes = obj.getJSONArray("allGenes");
+			for (int i = 0; i < jgenes.length(); i++){
+				Gene gene = new Gene(jgenes.getDouble(i));
+				genes.add(gene);
+			}
+
+			// Creating ArrayList of Individuals
+			ArrayList<Individual> individuals = new ArrayList<>();
+			Iterator geneIterator = genes.iterator();
+			while (geneIterator.hasNext()){
+				Gene[] geneArray = new Gene[this.genotypeSize];
+				for (int i = 0; i < this.genotypeSize; i++) {
+					geneArray[i] = (Gene) geneIterator.next();
+				}
+				Individual individual = new Individual(geneArray);
+				individuals.add(individual);
+			}
+
+			// Creating Population
+			this.population = new Population();
+			this.population.setIndividuals(individuals);
+			this.population.setGeneration(1);
 		}
 	}
 
