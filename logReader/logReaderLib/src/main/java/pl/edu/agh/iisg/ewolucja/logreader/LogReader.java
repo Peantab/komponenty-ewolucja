6707c33a8abe8d3f6f509f3bfd2af43603ae352f
imthumbelina <aga.zmc@gmail.com>
2019-06-02 18:44:06 +0200

    Add command line reader app

--- logReader/logReaderLib/src/main/java/pl/edu/agh/iisg/ewolucja/logreader/LogReader.java
+++ logReader/logReaderLib/src/main/java/pl/edu/agh/iisg/ewolucja/logreader/LogReader.java
@@ -86,6 +86,35 @@ public class LogReader {
         return stringBuilder.toString();
     }
 
+    public String buildJson(int run_id, String timeFrom, String timeTo){
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("{");
+        stringBuilder.append("\"query\"");
+        stringBuilder.append(": ");
+        stringBuilder.append("{");
+        stringBuilder.append("\"range\" :");
+        stringBuilder.append("{");
+
+        stringBuilder.append("\"timestamp\"");
+        stringBuilder.append(":");
+        stringBuilder.append("{");
+        stringBuilder.append("\"gte\"");
+        stringBuilder.append(": \"");
+        stringBuilder.append(timeFrom);
+        stringBuilder.append("\",");
+        stringBuilder.append("\"lte\"");
+        stringBuilder.append(": \"");
+        stringBuilder.append(timeTo);
+        stringBuilder.append("\"");
+        stringBuilder.append("}");
+        stringBuilder.append("}");
+        stringBuilder.append("}");
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+
     public void printResponse(String response){
         JSONObject jsonObject = new JSONObject(response);
         JSONObject hits = jsonObject.getJSONObject("hits");
@@ -93,6 +122,9 @@ public class LogReader {
         for(int i =0 ; i< arrayHits.length(); i++){
             System.out.print(arrayHits.getJSONObject(i).getJSONObject("_source"));
             System.out.print("\n");
+        }
+        if(arrayHits.length() == 0){
+            System.out.print("No results\n");
         }
     }
 
@@ -122,7 +154,29 @@ public class LogReader {
         con.disconnect();
         return null;
     }
-    public List<String> getLogs(Instant timeFrom, Instant timeTo){
+    public List<String> getLogs(String indexName, String timeFrom, String timeTo) throws IOException {
+        URL url = new URL("http://localhost:9200/" + indexName + "/_search");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        OutputStream os = con.getOutputStream();
+
+        String json = buildJson(999, timeFrom, timeTo);
+        os.write(json.getBytes(StandardCharsets.UTF_8));
+        os.flush();
+        os.close();
+        con.connect();
+        int code = con.getResponseCode();
+        if (code < 200 || code >= 300){
+            con.disconnect();
+            throw new RuntimeException("ElasticSearch returned code: " + code);
+        }
+//		FOR DEBUG:
+        byte[] bajty = new byte[1000];
+        con.getInputStream().read(bajty);
+        printResponse(new String(bajty));
+        con.disconnect();
         return null;
     }
     public List<String> getLogs(String moduleName){
--- logReader/logReaderLib/src/main/java/pl/edu/agh/iisg/ewolucja/logreader/Main.java
+++ logReader/logReaderLib/src/main/java/pl/edu/agh/iisg/ewolucja/logreader/Main.java
@@ -1,12 +1,50 @@
 package pl.edu.agh.iisg.ewolucja.logreader;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		LogReader logReader = new LogReader();
-		logReader.getLogs(99989,"general2");
-		logReader.getLastLogs(1, "general2", 99989);
+	    String usage = "[LOG READER APP]\n" +
+                "[USAGE function args]\n" +
+                "getLogs run_id index_name \n" +
+                "getLastLogs number_of_logs index_name run_id \n" +
+                "getLogsFromTo index_name timeFrom timeTo \n";
+        System.out.print(usage);
+        LogReader logReader = new LogReader();
+        Scanner scanner = new Scanner(System.in);
+	    while(true){
+	        System.out.print("Enter function name: ");
+            String functionName = scanner.next();
+            if(functionName.equals("getLogs")){
+                System.out.print("Enter run_id: ");
+                String run_id = scanner.next();
+                int id = Integer.parseInt(run_id);
+                System.out.print("Enter index_name: ");
+                String index_name = scanner.next();
+                logReader.getLogs(id, index_name);
+
+            }else if(functionName.equals("getLastLogs")){
+                System.out.print("Enter run_id: ");
+                String run_id = scanner.next();
+                int id = Integer.parseInt(run_id);
+                System.out.print("Enter index_name: ");
+                String index_name = scanner.next();
+                System.out.print("Enter number_of_logs: ");
+                int count = Integer.parseInt(scanner.next());
+                logReader.getLastLogs(count, index_name, id);
+            }else if(functionName.equals("getLogsFromTo")){
+                System.out.print("Enter index_name: ");
+                String index_name = scanner.next();
+                System.out.print("Enter timestamp from: ");
+                String timeFrom = scanner.next();
+                System.out.print("Enter timestamp to: ");
+                String timeTo = scanner.next();
+                logReader.getLogs(index_name, timeFrom, timeTo);
+            }else{
+                System.out.print(usage);
+            }
+        }
 	}
 }
